a63ee8349324d5e112df88cc73689274f46dc037
Cesar L <cgblaky@gmail.com>
2022-10-04 11:10:29 -0600

    Gallery
    the process work

--- amcrlimpia/src/main.jsx
+++ amcrlimpia/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Nav from '../src/components/navbar/Nav'
 import { Footer } from './components/Footer/Footer'
+import  Gallery  from './components/Gallery/Gallery'
 import Home from './components/Home/Home'
 
 
@@ -9,6 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Nav />
     <Home/>
+    <Gallery />
     <Footer/>
   </React.StrictMode>
 )
