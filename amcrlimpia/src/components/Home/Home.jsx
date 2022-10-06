a2fa17f1d4af9e5069694a41325c019a9323e655
Cesar L <cgblaky@gmail.com>
2022-10-05 19:04:40 -0600

    imagenes

--- amcrlimpia/src/components/Home/Home.jsx
+++ amcrlimpia/src/components/Home/Home.jsx
@@ -15,7 +15,7 @@ function Home() {
             <p class="lead">Empresa dedicada principalmente a la comercialización de chatarra ferrosa de exportación, ubicada en Costa Rica, provincia de Alajuela. Contamos con personal administrativo con más de 14 años de experiencia en el manejo de chatarra ferrosa y no ferrosa.</p>
         </div>
         <div class="col-md-5">
-            <img src="../src/components/Home/hom/truckWithContainer.jpg" alt="" id="homeImagesSQMetal" class="imgHomeBody01 rounded"/>
+            <img src="../src/components/Home/hom/lo3.jpeg" alt="" id="homeImagesSQMetal" class="imgHomeBody01 rounded"/>
         </div>
     </div>
 
@@ -27,7 +27,7 @@ function Home() {
                 <p class="lead">Am Costa Rica tiene una amplia experiencia en el ámbito de la gestión medioambiental de las empresas. Am Costa Rica cuenta con más de 15 años de experiencia en el área de la gestión ambiental. Contamos con los permisos requeridos para el desarrollo de la actividad comercial en nuestro país, lo que da soporte al usuario del servicio que realizó una correcta disposición de sus materiales para el reciclaje. Nuestras instalaciones de recepción, el personal y los equipos son adecuados para el manejo de los residuos. Nos caracterizamos por mantener un ambiente ordenado en nuestras instalaciones. No participamos en la compra de materiales ilícitos y prohibidos por nuestro país.</p>
             </div>
             <div class="col-md-5 order-md-1">
-                <img src="../src/components/Home/hom/truckWithContainer.jpg" alt="" id="homeImagesSQMetal" class="imgHomeBody02 rounded"/>
+                <img src="../src/components/Home/hom/lo2.jpeg" alt="" id="homeImagesSQMetal" class="imgHomeBody02 rounded"/>
 
             </div>
         </div>
@@ -46,7 +46,7 @@ function Home() {
                 en cuanto a impurezas.</p>
                 </div>
                 <div class="col-md-5">
-                    <img src="../src/components/Home/hom/truckWithContainer.jpg" alt="" id="homeImagesSQMetal" class="imgHomeBody03 rounded"/>
+                    <img src="../src/components/Home/hom/lo1.jpeg" alt="" id="homeImagesSQMetal" class="imgHomeBody03 rounded"/>
 
                 </div>
             </div>
