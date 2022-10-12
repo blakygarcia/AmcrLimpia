b6e97d06d73cfd583dfdf4aff4076f8bea21ec37
Cesar <cgblaky@gmail.com>
2022-10-12 15:47:33 -0600

    Gallery
    tested

--- amcrlimpia/src/components/Gallery/Gallery.jsx
+++ amcrlimpia/src/components/Gallery/Gallery.jsx
@@ -1,29 +1,74 @@
-import React from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import './Gallery.css'
-import './Swiper.js'
-const Gallery = () => {
-  return (
-    <div class="swiper">
-   
-    <div class="swiper-wrapper">
-      
-      <div class="swiper-slide">Slide 1</div>
-      <div class="swiper-slide">Slide 2</div>
-      <div class="swiper-slide">Slide 3</div>
-      ...
-    </div>
-    
-    <div class="swiper-pagination"></div>
-  
-   
-    <div class="swiper-button-prev"></div>
-    <div class="swiper-button-next"></div>
-  
-    
-    <div class="swiper-scrollbar"></div>
-  </div>
-  )
-}
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
-export default Gallery
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import './Gallery.css'
+
+// import required modules
+import { EffectCoverflow, Pagination } from "swiper";
+
+export default function Gallery() {
+  return (
+    <>
+    <div className="row featurette" data-aos="fade-right" data-aos-easing="ease-in-sine">
+            <div class="col-md-6 order-md-4">
+                <h1 class="featurette-heading secondTitle">Galeria</h1>
+              
+            </div>
+            <div class="col-md-6 order-md-1">
+               
+
+            </div>
+            </div>
+      <Swiper
+      
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src="../src/images/1.jpeg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../src/images/2.jpeg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../src/images/3.jpeg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../src/images/4.jpeg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../src/images/5.jpeg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../src/images/6.jpeg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../src/images/7.jpeg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../src/images/8.jpeg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="../src/images/9.jpeg" />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
--- amcrlimpia/src/components/Home/Home.jsx
+++ amcrlimpia/src/components/Home/Home.jsx
@@ -22,19 +22,24 @@ function Home() {
     <hr class="featurette-divider"/>
 
         <div class="row featurette" data-aos="fade-right" data-aos-easing="ease-in-sine">
-            <div class="col-md-7 order-md-2">
-                <h2 class="featurette-heading secondTitle">¿Por Qué Elegirnos? </h2>
-                <p class="lead">Am Costa Rica tiene una amplia experiencia en el ámbito de la gestión medioambiental de las empresas. Am Costa Rica cuenta con más de 15 años de experiencia en el área de la gestión ambiental. Contamos con los permisos requeridos para el desarrollo de la actividad comercial en nuestro país, lo que da soporte al usuario del servicio que realizó una correcta disposición de sus materiales para el reciclaje. Nuestras instalaciones de recepción, el personal y los equipos son adecuados para el manejo de los residuos. Nos caracterizamos por mantener un ambiente ordenado en nuestras instalaciones. No participamos en la compra de materiales ilícitos y prohibidos por nuestro país.</p>
-            </div>
-            <div class="col-md-5 order-md-1">
-                <img src="../src/components/Home/hom/lo2.jpeg" alt="" id="homeImagesSQMetal" class="imgHomeBody02 rounded"/>
+           
+            <div class="col-md-5 order-md-4">
+                <img src="../src/components/Home/hom/lo2.jpeg" alt="" id="homeImagesSQMetal2" class="imgHomeBody02 rounded"/>
 
+            </div>
+            <div class="col-md-7 order-md-3">
+                <h2 class="featurette-heading secondTitle">¿Por Qué Elegirnos? </h2>
+                <p class="lead2">Am Costa Rica tiene una amplia experiencia en el ámbito de la gestión medioambiental de las empresas. Am Costa Rica cuenta con más de 15 años de experiencia en el área de la gestión ambiental. Contamos con los permisos requeridos para el desarrollo de la actividad comercial en nuestro país, lo que da soporte al usuario del servicio que realizó una correcta disposición de sus materiales para el reciclaje. Nuestras instalaciones de recepción, el personal y los equipos son adecuados para el manejo de los residuos. Nos caracterizamos por mantener un ambiente ordenado en nuestras instalaciones. No participamos en la compra de materiales ilícitos y prohibidos por nuestro país.</p>
             </div>
         </div>
 
       
 
             <hr class="featurette-divider"/>
+
+      
+
+
      </div>
            
    )
